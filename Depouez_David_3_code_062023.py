18e981224c3629ad40b1e161536af8323a220e83
vad8713 <138607372+vad8713@users.noreply.github.com>
2023-07-04 18:37:39 +0200

    Update Depouez_David_3_code_062023.py
    add code to pre format text

--- Depouez_David_3_code_062023.py
+++ Depouez_David_3_code_062023.py
@@ -1,4 +1,101 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 import streamlit as st
+import nltk
+nltk.download('punkt')
+nltk.download('omw-1.4')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+import re
 
-st.write("Hello World !!!")
+def tokenizer_fct(sentence) :
+    # print(sentence)
+    #sentence_clean = sentence.replace('-', ' ').replace('+', ' ').replace('/', ' ').replace('#', ' ')
+    sentence_clean = sentence.replace('-', ' ').replace('&', ' ').replace('/', ' ')
+    word_tokens = word_tokenize(sentence_clean)
+   
+    return word_tokens
+
+def retokenizer_fct(tokens) :
+    mwtokenizer = nltk.MWETokenizer(separator='')
+    mwtokenizer.add_mwe(('c', '#'))
+    word_tokens = mwtokenizer.tokenize(tokens)
+    
+    return word_tokens
+
+# Stop words
+from nltk.corpus import stopwords
+stop_w = list(set(stopwords.words('english'))) + ['[', ']', ',', '.', ':', '?', '(', ')',';','!','<','>']
+
+def stop_word_filter_fct(list_words) :
+    filtered_w = [w for w in list_words if not w in stop_w]
+    return filtered_w
+
+# lower case et alpha
+def lower_start_fct(list_words) :
+    lw = [w.lower() for w in list_words if (not w.startswith("@")) 
+    #                                   and (not w.startswith("#"))
+                                       and (not w.startswith("http"))]
+    return lw
+
+# Lemmatizer (base d'un mot)
+from nltk.stem import WordNetLemmatizer
+
+def lemma_fct(list_words) :
+    lemmatizer = WordNetLemmatizer()
+    lem_w = [lemmatizer.lemmatize(w) for w in list_words]
+    return lem_w
+
+# remove code and tags in pattern : <code> code </code>)
+def removeCodeMarkup(sentence):
+    codeMarkupRegEx = r'<code>(.*?)</code>'
+    #re.compile(codeMarkupRegEx,flag=re.DOTALL)
+    cleanText = re.sub(codeMarkupRegEx,'',sentence,flags=re.DOTALL)
+    return cleanText
+
+# remove html tags
+def removeHTML(sentence):
+    htmlMarkupRegEx = '<.*?>'
+    cleanText = re.sub(htmlMarkupRegEx,'',sentence)
+    return cleanText
+
+# Fonction de préparation du texte pour le bag of words (Countvectorizer et Word2Vec)
+def transform_bow_fct(desc_text) :
+    sentence = removeCodeMarkup(desc_text)
+    sentence = removeHTML(sentence)
+    word_tokens = tokenizer_fct(sentence)
+    lw = lower_start_fct(word_tokens)
+    sw = stop_word_filter_fct(lw)
+    re_sw = retokenizer_fct(sw)
+    # lem_w = lemma_fct(re_sw)    
+    transf_desc_text = ' '.join(re_sw)
+    return transf_desc_text
+
+# Fonction de préparation du texte pour le bag of words avec lemmatization
+def transform_bow_lem_fct(desc_text) :
+    sentence = removeCodeMarkup(desc_text)
+    sentence = removeHTML(sentence)
+    word_tokens = tokenizer_fct(sentence)
+    lw = lower_start_fct(word_tokens)
+    sw = stop_word_filter_fct(lw)
+    re_sw = retokenizer_fct(sw)
+    lem_w = lemma_fct(re_sw)    
+    transf_desc_text = ' '.join(lem_w)
+    return transf_desc_text
+
+# Fonction de préparation du texte pour le Deep learning (USE et BERT)
+def transform_dl_fct(desc_text) :
+    sentence = removeCodeMarkup(desc_text)
+    sentence = removeHTML(sentence)
+    word_tokens = tokenizer_fct(sentence)
+#    sw = stop_word_filter_fct(word_tokens)
+    lw = lower_start_fct(word_tokens)
+    # lem_w = lemma_fct(lw)    
+    transf_desc_text = ' '.join(lw)
+    return transf_desc_text
+
+st.write("""
+# Test app
+""")
+
